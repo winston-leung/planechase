7c1b8ce30083feab04aa9e96f9013146f0c49077
“Winston <winston.leung1465@gmail.com>
2023-07-01 13:35:01 -0400

    dot

--- src/App.js
+++ src/App.js
@@ -5,76 +5,11 @@ import planes from "./planes.json";
 import Header from './Header';
 
 function App() {
-  const count = planes.length;
-  const [planeChecklist, setPlaneChecklist] = useState(planes.reduce((acc, cur) => {
-
-    acc.push({ name: cur.name, selected: true, id: cur.id });
-    acc.sort((a, b) => {
-      const nameA = a.name.toUpperCase();
-      const nameB = b.name.toUpperCase();
-      if (nameA < nameB) return -1;
-      if (nameA > nameB) return 1;
-      return 0;
-    });
-    return acc;
-  }, []))
-  const checkedCount = planeChecklist.reduce((acc, cur) => {
-    if (cur.selected) acc++;
-    return acc;
-  }, 0)
-  const [imageURL, setImageURL] = useState("");
-
-  const handleHover = (e) => {
-    const sel = planes.find(plane => plane.id === e.currentTarget.id)
-
-    if (sel) setImageURL(sel.image_uris.large);
-  }
-  const handleCheck = (e) => {
-    const newList = [...planeChecklist];
-    newList.forEach(line => {
-      if (line.id === e.target.id) line.selected = e.target.checked;
-    })
-    setPlaneChecklist(newList);
-  }
-
-  const handleSelectAll = (e) => {
-    e.preventDefault();
-    const newList = [...planeChecklist];
-    newList.forEach(line => {
-      line.selected = true;
-    })
-    setPlaneChecklist(newList);
-  }
-
   return (
     <>
       <Header />
-      <div className='main'>
-        <div className='sidebar'>
-          <form className='form'>
-            <div className='buttons'>
-              <button>Start</button>
-              <button onClick={handleSelectAll}>Select All</button>
-            </div>
-            Selected: {checkedCount} of {count}
-            {planeChecklist.map(list => {
-              return (
-                <div onMouseOver={handleHover} id={list.id} key={list.name}>
-                  <input type='checkbox' checked={list.selected} onChange={handleCheck} id={list.id} />
-                  <label className='plane'  >{list.name}</label>
-                </div>
-              )
-            })}
-          </form>
-        </div >
-        <div className='maindiv'>
-          <div className='imgdiv'>
-            {imageURL ? <img className='preview' src={imageURL} /> : ""}
-          </div>
-        </div>
-      </div >
     </>
-  );
+  )
 }
 
 export default App;
