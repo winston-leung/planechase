03aaa992ed4e28d36910b3b73b3c66e6e4d00cf4
“Winston <winston.leung1465@gmail.com>
2023-07-06 20:52:36 -0400

    2.0

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import { useState } from 'react';
 import './App.css';
 import planes from "./planes.json";
 import Header from './Header';
+import Main from './Main';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-    </>
+      <Routes>
+        <Route exact path="/" element={<Main />} />
+        <Route path="/:path" element={<div></div>} />
+        <Route path="">404: Oops!</Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
