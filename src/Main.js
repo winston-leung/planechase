4f3eaded4be74f97414098474e9806c4c3bb13cb
Winston Leung <winston.leung1465@gmail.com>
2023-07-20 14:49:22 -0400

    ffff

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,34 @@
-
+import { useState } from 'react';
+import './Main.css';
+import planes from './planes.json';
 
 const Main = () => {
-  return (
-    <div>
+  const [list, setList] = useState(planes.reduce((acc, cur, ind) => {
+    acc.push({ [ind]: true });
+    return acc;
+  }, []))
 
+
+  const handleSearcChange = {}
+
+
+
+
+  return (
+    <div className="mainbody">
+      <input type="box" className='searchbox' onChange={null} />
+      <div className='grid'>
+        {list.map((plane, ind) => {
+          console.log(plane)
+          if (plane) {
+            return (
+              <div key={ind} className='card'>{planes[Object.keys(plane)].name}</div>
+            )
+          } else {
+            return <div />
+          }
+        })}
+      </div>
     </div>
   )
 }
